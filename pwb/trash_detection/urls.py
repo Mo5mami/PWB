d8859114934503da4ec54d05f92641343cbe8aec
mo5mami <mami.mokhtar123@gmail.com>
2021-04-22 04:40:15 +0100

    making django async 2

--- pwb/trash_detection/urls.py
+++ pwb/trash_detection/urls.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 from django.conf.urls import url
-
+from asgiref.sync import sync_to_async
 
 
 urlpatterns = [
-    path(r'trash-detection/inference/', views.TrashDetection.as_view()),
+    path(r'trash-detection/inference/', sync_to_async(views.TrashDetection.as_view())),
     
 ]
 
--- pwb/trash_detection/views.py
+++ pwb/trash_detection/views.py
@@ -3,15 +3,17 @@ from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from .trash_detection_service import TrashDetectionService
 from asgiref.sync import sync_to_async
+import asyncio
 
 class TrashDetection(APIView):
     async def __call__():
         pass 
-        
+
     def post(self, request, format=None):
         """
         Demand inference on the object detection model
         """
+        print("am I async : ",asyncio.iscoroutinefunction(self.post))
         return TrashDetectionService.inference(request)
     
         
