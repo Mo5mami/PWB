207ed35b2af3f2a1e27665acc75d152e7b93f614
mo5mami <mami.mokhtar123@gmail.com>
2021-04-22 04:26:56 +0100

    make django async

--- pwb/trash_detection/trash_detection_service.py
+++ pwb/trash_detection/trash_detection_service.py
@@ -8,13 +8,16 @@ from .utils import FixFormat
 from .serializers import TrashDetectionSerializer
 from django.core.files import File 
 from django.core.files.images import ImageFile 
+from asgiref.sync import sync_to_async
 
 class TrashDetectionService:
+    
     
     @classmethod
     def trash_detection_inference_url(cls , ):
         return os.path.join(settings.TORCHSERVE_URL , "predictions/trash_detection")
 
+    
     @classmethod
     def trash_detection_request(cls , data):
 
@@ -23,6 +26,7 @@ class TrashDetectionService:
         prediction = json.loads(response.content.decode())
         return Response(prediction,status=response.status_code,)
 
+    
     @classmethod
     def inference(cls , request):
         print("request : ",request)
--- pwb/trash_detection/views.py
+++ pwb/trash_detection/views.py
@@ -2,12 +2,16 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from .trash_detection_service import TrashDetectionService
-
+from asgiref.sync import sync_to_async
 
 class TrashDetection(APIView):
+    async def __call__():
+        pass 
+        
     def post(self, request, format=None):
         """
         Demand inference on the object detection model
         """
         return TrashDetectionService.inference(request)
+    
         
