2cac32fdf4d243f13fc1b4895a63a082ffb0cf3b
mo5mami <mami.mokhtar123@gmail.com>
2021-04-25 22:21:52 +0100

    change interaction

--- pwb/pwb/settings/prod.py
+++ pwb/pwb/settings/prod.py
@@ -3,7 +3,7 @@ from .base import *
 DEBUG = False
 
 #TORCHSERVE_URL = "https://torchservemo5.azurewebsites.net/"
-TORCHSERVE_URL ="http://54.162.113.184/"
+TORCHSERVE_URL = os.environ['TORCHSERVE_URL']
 """ALLOWED_HOSTS = [
     "https://personalwebsitemo5.vercel.app",
     "http://127.0.0.1:3000/"
@@ -13,7 +13,7 @@ ALLOWED_HOSTS = [r'*']
 
 CORS_ALLOWED_ORIGINS = [
     "https://personalwebsitemo5.vercel.app",
-    "http://127.0.0.1:3000"
+    #"http://127.0.0.1:3000"
 ]
 CORS_ALLOW_ALL_ORIGINS = False
 
